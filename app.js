ed7f33bdd93e3893d3b184b90d1bea89c4164029
Alison de la Paz <arhibschle@gmail.com>
2025-06-11 12:45:09 -0400

    more ESLint updates

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5d8b8592978f8bd833ca8133",
+  };
+  next();
+});
 app.use("/", mainRouter); // if requests are sent to / then send to userRouter
 
 app.use((req, res) => {
@@ -15,7 +22,6 @@ app.use((req, res) => {
     message: "Requested resource not found",
   });
 });
-
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
