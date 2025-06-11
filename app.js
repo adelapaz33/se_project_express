c2ba6b65cfbcf75f106a32b75ddca8ccd58afc48
Alison de la Paz <arhibschle@gmail.com>
2025-06-11 12:48:42 -0400

    trial update

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const { PORT = 3001 } = process.env;
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
-  };
-  next();
-});
+
 app.use("/", mainRouter); // if requests are sent to / then send to userRouter
 
 app.use((req, res) => {
